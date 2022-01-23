8ccda0bcba2ecb780b665e45c7abf53b284a1c04
Makshe Seytkaliev <3meuka0@gmail.com>
2022-01-23 14:01:34 +0300

    initial commit

---
+++ cmake-build-debug/CMakeFiles/Compiler.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Compiler.dir/main.cpp.obj"
+  "Compiler.exe"
+  "Compiler.exe.manifest"
+  "Compiler.pdb"
+  "libCompiler.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Compiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
