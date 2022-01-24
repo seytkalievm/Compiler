bebe9a0f15415be085469e2df73de8f813380b09
Makshe Seytkaliev <3meuka0@gmail.com>
2022-01-24 15:35:44 +0300

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 
+
+bool isPunctuator(char ch)					//check if the given character is a punctuator or not
+{
+    if (ch == ' ' || ch == ':' || ch == '(' || ch == ')' || ch == '{' ||
+        ch == '}' || ch == '|' || ch == '+' || ch == '-' || ch == '.' )
+    {
+        return true;
+    }
+    return false;
+}
+
+
+
 int main() {
     cout << "Hello, World!" << endl;
     return 0;
